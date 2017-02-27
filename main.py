f6ec64ff2507654c86b6c4153a05e845e367dc03
Cregg Hancock <creggh@gmail.com>
2017-02-27 16:39:28 -0600

    completed user signup with error messages

--- main.py
+++ main.py
@@ -15,11 +15,145 @@
 # limitations under the License.
 #
 import webapp2
+import cgi
+from cgi import escape
+
+
+def valid_user(name):
+    if ' ' not in name:
+        return name
+
+def valid_pass(pass_w):
+    if pass_w:
+        return pass_w
+
+def valid_valid(pass_v, pass_w):
+    if pass_v:
+        if pass_v == pass_w:
+            return pass_v
+
+def valid_email(email):
+    if '@' in email:
+        if email[-4:]== ".com":
+            return email
+
+
+# html boilerplate for the top of every page
+page_header = """
+<!DOCTYPE html>
+<html>
+<head>
+    <title>User-Signup</title>
+    <style type="text/css">
+        .error {
+            color: red;
+        }
+    </style>
+</head>
+<body>
+    <h1>
+        <a href="/">User-Signup</a>
+    </h1>
+"""
+
+# html boilerplate for the bottom of every page
+page_footer = """
+</body>
+</html>
+"""
+main_post = """
+<form method = "post">
+    <table>
+        <tbody>
+        <tr>
+            <td>
+            <label for="username">Username</label>
+            </td>
+            <td>
+            <input name="username" type ="text" value = "%(username)s">
+            </td>
+            <td class ="error" style ="color: red">%(user_error)s</td>
+
+        </tr>
+        <tr>
+            <td>
+            <label for="password">Password</label>
+            </td>
+            <td>
+            <input name="password" type="password">
+            </td>
+            <td class ="error" style ="color: red">%(pass_error)s</td>
+
+        </tr>
+        <tr>
+            <td>
+            <label for="verify">Password</label>
+            </td>
+            <td>
+            <input name="verify" type="password">
+            </td>
+            <td class = "error" style ="color: red">%(verify_error)s</td>
+
+        </tr>
+        <tr>
+            <td>
+            <label for="email">Email(optional)</label>
+            <td>
+            <input name="email" type="email" value = "%(email)s">
+            </td>
+            <td class ="error" style ="color: red">%(email_error)s</td>
+        </tr>
+        </tbody>
+    </table>
+    <input type = "submit">
+</form>
+
+"""
+
 
 class MainHandler(webapp2.RequestHandler):
+    def write_form(self, user_error="", username="", email="", pass_error="", verify_error="", email_error=""):
+        self.response.out.write(page_header + main_post % {"user_error": user_error,
+                                            "username": username,
+                                            "email" : email,
+                                            "pass_error": pass_error,
+                                            "verify_error": verify_error,
+                                            "email_error": email_error
+                                            } + page_footer)
+
     def get(self):
-        self.response.write('Hello world!')
+        self.write_form()
+
+    def post(self):
+                user_username = valid_user(self.request.get("username"))
+                user_password = valid_pass(self.request.get("password"))
+                user_validated = valid_valid(self.request.get("verify"), self.request.get("password"))
+                user_email = valid_email(self.request.get("email"))
+
+                user_error = ""
+                pass_error = ""
+                verify_error = ""
+                email_error = ""
+
+                if not user_username:
+                    user_error = "Username invalid"
+                if not user_password:
+                    pass_error = "Invalid Password"
+                if not user_validated:
+                    verify_error = "Invalid Password"
+                if not user_email:
+                    email_error = "Invalid Email"
+
+
+                if not (user_username and user_password and user_validated and user_email):
+                    self.write_form(user_error, user_username, user_email, pass_error, verify_error, email_error)
+                else:
+                    self.redirect("/welcome")
+
+class WelcomeHandler(webapp2.RequestHandler):
+    def get(self):
+        self.response.out.write("Welcome!")
 
 app = webapp2.WSGIApplication([
-    ('/', MainHandler)
+    ('/', MainHandler), ('/welcome', WelcomeHandler)
 ], debug=True)
